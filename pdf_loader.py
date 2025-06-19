680b387757b291a7ef753cf757658ca44f98260f
Amit Kumar Mondal <mamit5620@gmail.com>
2025-06-19 16:57:13 +0530

    update pdf_loader.py file

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,5 +1,29 @@
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
+
+# def load_pdf(file_path):
+#     loader = PyPDFLoader(file_path)
+#     return loader.load()
+
+
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders.parsers import TesseractBlobParser
 
 def load_pdf(file_path):
-    loader = PyPDFLoader(file_path)
-    return loader.load()
+    # loader = PyMuPDFLoader(file_path=file_path, extract_images=True)
+    # documents = loader.load()
+    # return documents
+
+    loader = PyMuPDFLoader(
+    file_path=file_path,
+    mode="page",
+    extract_tables="markdown",
+    images_inner_format="html-img",
+    images_parser=TesseractBlobParser()
+    )
+
+    docs = loader.load()
+    return docs
+
+
+
+
